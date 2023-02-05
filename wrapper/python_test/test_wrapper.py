6827ab036181018d5149bb5ee6866151528556b9
gareth-cross <gcross.code@icloud.com>
2023-02-05 11:47:28 -0800

    Add yapf to pre-commit, format test_wrapper

--- wrapper/python_test/test_wrapper.py
+++ wrapper/python_test/test_wrapper.py
@@ -20,9 +20,10 @@ def assert_not_identical(a: mc.Expr, b: mc.Expr):
     """Assert that two expressions are not identical."""
     if not a.is_identical_to(b):
         return
-    message = (f'The expressions a=`{repr(a)}` b=`{repr(b)}` are identical (they should not be).\n' +
-               f'The expression tree for `a` is:\n{a.expression_tree_str()}\n' +
-               f'The expression tree for `b` is:\n{b.expression_tree_str()}')
+    message = (
+        f'The expressions a=`{repr(a)}` b=`{repr(b)}` are identical (they should not be).\n' +
+        f'The expression tree for `a` is:\n{a.expression_tree_str()}\n' +
+        f'The expression tree for `b` is:\n{b.expression_tree_str()}')
     raise AssertionError(message)
 
 
