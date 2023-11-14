b7e0b78530f3fad57c3ee0f1a5211af96d2acd8c
gareth-cross <gcross.code@icloud.com>
2023-11-14 12:22:52 -0800

    Add tests to quaternion interpolation example

--- components/math/examples/quaternion_interpolation/quat_interpolation_gen.cc
+++ components/math/examples/quaternion_interpolation/quat_interpolation_gen.cc
@@ -4,35 +4,7 @@
 
 #include "code_gen_helpers.h"
 #include "code_generation/cpp_code_generator.h"
-#include "geometry/quaternion.h"
-#include "type_annotations.h"
-
-using namespace math;
-namespace ta = math::type_annotations;
-
-// Interpolate between two quaternions, `q0` and `q1` (passed as scalar-first vectors).
-// Outputs the interpolated quaternion, as well as the tangent space
-auto quaternion_interpolation(ta::StaticMatrix<4, 1> q0_vec, ta::StaticMatrix<4, 1> q1_vec,
-                              Expr alpha) {
-  using namespace matrix_operator_overloads;
-
-  const Quaternion q0 = Quaternion::from_vector_wxyz(q0_vec);
-  const Quaternion q1 = Quaternion::from_vector_wxyz(q1_vec);
-  const MatrixExpr q_delta_tangent = (q0.conjugate() * q1).to_rotation_vector(1.0e-16);
-  const Quaternion q_interp =
-      q0 * Quaternion::from_rotation_vector(q_delta_tangent * alpha, 1.0e-16);
-
-  ta::StaticMatrix<3, 3> D_q0 = q_interp.right_local_coordinates_derivative() *
-                                q_interp.to_vector_wxyz().jacobian(q0_vec) *
-                                q0.right_retract_derivative();
-  ta::StaticMatrix<3, 3> D_q1 = q_interp.right_local_coordinates_derivative() *
-                                q_interp.to_vector_wxyz().jacobian(q1_vec) *
-                                q1.right_retract_derivative();
-
-  return std::make_tuple(OutputArg("q_out", q_interp.to_vector_wxyz()),
-                         OptionalOutputArg("D_q0", std::move(D_q0)),
-                         OptionalOutputArg("D_q1", std::move(D_q1)));
-}
+#include "quat_interpolation_expressions.h"
 
 int main() {
   using namespace math;
--- components/math/examples/quaternion_interpolation/quat_interpolation_test.cc
+++ components/math/examples/quaternion_interpolation/quat_interpolation_test.cc
@@ -1,10 +1,70 @@
 // Copyright 2023 Gareth Cross
 #include "eigen_test_helpers.h"
 #include "numeric_testing.h"
+#include "numerical_jacobian.h"
 #include "test_helpers.h"
 #include "type_annotations.h"
+
+#include "quat_interpolation_expressions.h"
 
 #define MATH_SPAN_EIGEN_SUPPORT
 #include "span_eigen.h"
 
 #include "generated.h"
+
+namespace math {
+
+using Eigen::AngleAxisd;
+using Eigen::Matrix3d;
+using Eigen::Quaterniond;
+using Eigen::Vector3d;
+
+Vector3d local_coordinates(const Quaterniond& a, const Quaterniond& b) {
+  const AngleAxisd w_ab{a.inverse() * b};
+  return w_ab.angle() * w_ab.axis();
+}
+
+Quaterniond retract(const Quaterniond& q, const Vector3d& w) {
+  return q * Quaterniond{AngleAxisd{w.norm(), w.normalized()}};
+}
+
+Quaterniond quat_interp(const Quaterniond& a, const Quaterniond& b, const double alpha) {
+  return retract(a, local_coordinates(a, b) * alpha);
+}
+
+// Test the quaternion interpolation result numerically.
+TEST(QuaternionInterpolationTest, TestQuatInterpolation) {
+  auto evaluator = create_evaluator(&math::quaternion_interpolation);
+
+  const Quaterniond q0 = Quaterniond{AngleAxisd(M_PI / 3, Vector3d::UnitX())} *
+                         Quaterniond{AngleAxisd(M_PI / 6, Vector3d::UnitZ())};
+  const Quaterniond q1{AngleAxisd(-M_PI / 4, Vector3d::UnitY())};
+
+  Quaterniond q_eval{};
+  Matrix3d D0_eval, D1_eval;
+  evaluator(q0.coeffs(), q1.coeffs(), 0.25, q_eval.coeffs(), D0_eval, D1_eval);
+
+  Quaterniond q_gen{};
+  Matrix3d D0_gen{}, D1_gen{};
+  gen::quaternion_interpolation(q0, q1, 0.25, q_gen, D0_gen, D1_gen);
+
+  EXPECT_EIGEN_NEAR(q_eval.coeffs(), q_gen.coeffs(), 2.0e-16);
+  EXPECT_EIGEN_NEAR(D0_eval, D0_gen, 2.0e-16);
+  EXPECT_EIGEN_NEAR(D1_eval, D1_gen, 2.0e-16);
+
+  // compare to interpolation with eigen
+  EXPECT_EIGEN_NEAR(quat_interp(q0, q1, 0.25).coeffs(), q_gen.coeffs(), 2.0e-16);
+
+  // compute derivatives numerically
+  const Matrix3d D0_num = numerical_jacobian(Vector3d::Zero().eval(), [&](const Vector3d& w) {
+    return local_coordinates(quat_interp(q0, q1, 0.25), quat_interp(retract(q0, w), q1, 0.25));
+  });
+  const Matrix3d D1_num = numerical_jacobian(Vector3d::Zero().eval(), [&](const Vector3d& w) {
+    return local_coordinates(quat_interp(q0, q1, 0.25), quat_interp(q0, retract(q1, w), 0.25));
+  });
+
+  EXPECT_EIGEN_NEAR(D0_num, D0_gen, 1.0e-12);
+  EXPECT_EIGEN_NEAR(D1_num, D1_gen, 1.0e-12);
+}
+
+}  // namespace math
