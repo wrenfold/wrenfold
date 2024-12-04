026aff8cec4fb70f2a6646168418742a99988e42
gareth-cross <gcross.code@icloud.com>
2024-12-03 19:23:26 -0800

    Stubs are generated into the repo and commit to components/wrapper/stubs

---
+++ components/wrapper/stubs/pywrenfold/__init__.pyi
@@ -0,0 +1,6 @@
+from . import ast as ast, enumerations as enumerations, exceptions as exceptions, expressions as expressions, gen as gen, geometry as geometry, sym as sym, sympy_conversion as sympy_conversion, type_info as type_info
+
+__git_version__: str
+__version__: str
+
+def set_tracing_output_path(path: str) -> None: ...
