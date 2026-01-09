8c961ba80a13546d4d018222a098c85bc7f3408d
gareth-cross <gcross.code@icloud.com>
2026-01-09 00:41:39 -0800

    Add some imports to __init__.py

--- components/python/wrenfold/__init__.py
+++ components/python/wrenfold/__init__.py
@@ -1,7 +1,45 @@
 """Symbolic code-generation tools."""
 
-from pywrenfold import __version__  # noqa: F401
+# ruff: noqa: F401
+
+from pywrenfold import __version__
 
 __author__ = "Gareth <gcross.code@icloud.com>"
 __copyright__ = "Copyright (C) 2024 Gareth Cross"
 __license__ = "MIT"
+
+
+from .code_generation import (
+    CppGenerator,
+    OutputArg,
+    PythonGenerator,
+    PythonGeneratorTarget,
+    ReturnValue,
+    RustGenerator,
+    generate_function,
+    generate_python,
+    mkdir_and_write_file,
+)
+from .enumerations import NumberSet, RelationalOperation, StdMathFunction, SymbolicConstant
+from .type_annotations import (
+    FloatScalar,
+    IntScalar,
+    Matrix1,
+    Matrix2,
+    Matrix3,
+    Matrix4,
+    Matrix5,
+    Matrix6,
+    Matrix7,
+    Matrix8,
+    Matrix9,
+    Vector1,
+    Vector2,
+    Vector3,
+    Vector4,
+    Vector5,
+    Vector6,
+    Vector7,
+    Vector8,
+    Vector9,
+)
